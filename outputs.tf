63c8a63810ffc949aa719abb85fe9fdba9dafca3
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:16:10 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,20 @@ output "web_pubsub_custom_domains" {
   description = "All web_pubsub_custom_domain resources"
   value       = azurerm_web_pubsub_custom_domain.web_pubsub_custom_domains
 }
+output "web_pubsub_custom_domains_domain_name" {
+  description = "List of domain_name values across all web_pubsub_custom_domains"
+  value       = [for k, v in azurerm_web_pubsub_custom_domain.web_pubsub_custom_domains : v.domain_name]
+}
+output "web_pubsub_custom_domains_name" {
+  description = "List of name values across all web_pubsub_custom_domains"
+  value       = [for k, v in azurerm_web_pubsub_custom_domain.web_pubsub_custom_domains : v.name]
+}
+output "web_pubsub_custom_domains_web_pubsub_custom_certificate_id" {
+  description = "List of web_pubsub_custom_certificate_id values across all web_pubsub_custom_domains"
+  value       = [for k, v in azurerm_web_pubsub_custom_domain.web_pubsub_custom_domains : v.web_pubsub_custom_certificate_id]
+}
+output "web_pubsub_custom_domains_web_pubsub_id" {
+  description = "List of web_pubsub_id values across all web_pubsub_custom_domains"
+  value       = [for k, v in azurerm_web_pubsub_custom_domain.web_pubsub_custom_domains : v.web_pubsub_id]
+}
 
