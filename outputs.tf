97b7a8cdd1b55a298371846ce6d03113993efe87
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:32:47 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "web_pubsub_custom_domains_id" {
+  description = "Map of id values across all web_pubsub_custom_domains, keyed the same as var.web_pubsub_custom_domains"
+  value       = { for k, v in azurerm_web_pubsub_custom_domain.web_pubsub_custom_domains : k => v.id }
+}
 output "web_pubsub_custom_domains_domain_name" {
   description = "Map of domain_name values across all web_pubsub_custom_domains, keyed the same as var.web_pubsub_custom_domains"
   value       = { for k, v in azurerm_web_pubsub_custom_domain.web_pubsub_custom_domains : k => v.domain_name }
